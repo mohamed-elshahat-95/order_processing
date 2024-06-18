b0318ba1d0a7dd1c42d625e641a0326d9f29b98f
Mohamed Elshahat <mohamed.elshahat@casita.com>
2024-06-18 21:05:19 +0300

    init my-app repo

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return response()->json([
+                'status' => false,
+                'msg' => 'Credentials are missing or not valid!'
+            ]);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token');
+        return ['token' => $token->plainTextToken];
+    }
+
+    public function getUsers()
+    {
+        $users = User::all();
+        return response()->json([
+            'status' => true,
+            'data' => $users
+        ]);
+    } 
+    
+    public function getMyData(Request $request)
+    {
+        return response()->json([
+            'status' => true,
+            'data' => $request->user()
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function () {
+    $ch = curl_init('https://coderbyte.com/api/challenges/json/age-counting');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+  
+    dd(json_decode($data, true));
+    
+});
