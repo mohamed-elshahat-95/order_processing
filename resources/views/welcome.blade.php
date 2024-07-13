c4517d9e211c6df5b338995858f61430648daa3b
Mohamed Elshahat <mohamed.elshahat@casita.com>
2024-07-13 15:47:04 +0300

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Me</title>
+  <title>Me...</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
-    .row.content {height: 1500px}
-    .sidenav {
-      background-color: #f1f1f1;
-      height: 100%;
-    }
     footer {
       background-color: #555;
       color: white;
       padding: 15px;
-    }
-    @media screen and (max-width: 767px) {
-      .sidenav {
-        height: auto;
-        padding: 15px;
-      }
-      .row.content {height: auto;} 
     }
   </style>
 </head>
